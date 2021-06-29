69c0bf045fcccbaf5779a5eb2d1e5c8081cafbac
Sam Sayer <ssayer@mitre.org>
2021-06-29 15:07:46 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>SMART Health Cards Web Verifier</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const titleElement = screen.getByText(/SMART Health Card/i);
+  expect(titleElement).toBeInTheDocument();
+});
