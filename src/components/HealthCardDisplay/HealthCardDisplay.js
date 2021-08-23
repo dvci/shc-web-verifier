83caac02efaf540f6819573e9e76e379ee6725ad
Daniel Lee <danlee1025@gmail.com>
2021-08-23 13:25:04 -0400

    Display list of immunizations (#2)
    * - Move components into separate directory
    - Create HealthCardDisplay component
    - Display immunization info in Cards
    
    * Add title
    
    * Fix justifyContent prop
    
    * Add catch when stopScanning causes an error.
    
    * Add test for HealthCardDisplay
    
    * Use relative imports.
    
    * Add eslint
    
    * Add github actions
    
    * Use fullUrl for key and add performer.
    
    * Remove leading and trailing whitespace from name.
    
    * Make display of lotNumber and performer conditional.

---
+++ src/components/HealthCardDisplay/HealthCardDisplay.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Card, CardContent, CardHeader, Grid, Typography
+} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  bold: {
+    fontWeight: 'bold'
+  }
+});
+
+const HealthCardDisplay = ({ patientData }) => {
+  const classes = useStyles();
+
+  const immunizationCode = (codings) => {
+    if (codings.length === 0) return '';
+
+    const coding = codings[0];
+    return coding.system ? `${coding.system}#${coding.code}` : coding.code;
+  };
+
+  const immunizationDisplay = (immunization) => {
+    const { fullUrl, resource } = immunization;
+
+    return (
+      <Grid item key={fullUrl}>
+        <Card variant="outlined">
+          <CardHeader
+            title={
+              resource.vaccineCode ? immunizationCode(resource.vaccineCode.coding) : ''
+            }
+            classes={{ title: classes.bold }}
+          />
+          <CardContent>
+            <Typography gutterBottom>
+              <span className={classes.bold}>Date: </span>
+              <span>{resource.occurrenceDateTime}</span>
+            </Typography>
+            {resource.lotNumber && (
+              <Typography gutterBottom>
+                <span className={classes.bold}>Lot: </span>
+                <span>{resource.lotNumber}</span>
+              </Typography>
+            )}
+            {(resource.performer && resource.performer.length > 0) && (
+              <Typography gutterBottom>
+                <span className={classes.bold}>Performer: </span>
+                <span>{resource.performer[0].actor.display}</span>
+              </Typography>
+            )}
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  }
+
+  return (
+    <div>
+      <Typography gutterBottom>
+        <span className={classes.bold}>Name: </span>
+        <span>{patientData.name}</span>
+      </Typography>
+      <Typography gutterBottom>
+        <span className={classes.bold}>Date of Birth: </span>
+        <span>{patientData.dateOfBirth}</span>
+      </Typography>
+      <Typography variant="h4"> Immunizations </Typography>
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+      >
+        {patientData.immunizations.map((i) => immunizationDisplay(i))}
+      </Grid>
+    </div>
+  );
+};
+
+export default HealthCardDisplay;
---
+++ src/components/HealthCardDisplay/HealthCardDisplay.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import HealthCardDisplay from './HealthCardDisplay';
+
+const patientData = {
+  name: 'John B. Anyperson',
+  dateOfBirth: '1951-01-20',
+  immunizations: [
+    {
+      fullUrl: 'resource:1',
+      resource: {
+        resourceType: 'Immunization',
+        status: 'completed',
+        vaccineCode: {
+          coding: [
+            {
+              system: 'http://hl7.org/fhir/sid/cvx',
+              code: '207'
+            }
+          ]
+        },
+        patient: {
+          reference: 'resource:0'
+        },
+        occurrenceDateTime: '2021-01-01',
+        performer: [
+          {
+            actor: {
+              display: 'ABC General Hospital'
+            }
+          }
+        ],
+        lotNumber: '0000001'
+      }
+    }
+  ]
+};
+
+test('renders health card', () => {
+  render(<HealthCardDisplay patientData={patientData} />);
+  const patientName = screen.getByText(/John B. Anyperson/i);
+  expect(patientName).toBeInTheDocument();
+  const immunizationDate = screen.getByText(/2021-01-01/i);
+  expect(immunizationDate).toBeInTheDocument();
+});
+
+test('renders health card without performer', () => {
+  // Remove performer from immunization
+  delete patientData.immunizations[0].resource.performer;
+  render(<HealthCardDisplay patientData={patientData} />);
+  const patientName = screen.getByText(/John B. Anyperson/i);
+  expect(patientName).toBeInTheDocument();
+  const immunizationDate = screen.getByText(/2021-01-01/i);
+  expect(immunizationDate).toBeInTheDocument();
+});
