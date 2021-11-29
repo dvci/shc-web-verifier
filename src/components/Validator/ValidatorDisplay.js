88b81a8495be021b25b770e8b75eb8f538c29788
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2021-11-29 13:27:01 -0500

    lint

--- src/components/Validator/ValidatorDisplay.js
+++ src/components/Validator/ValidatorDisplay.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { makeStyles } from '@mui/styles';
+import { Typography } from '@mui/material';
 import { Validator } from './Validator.tsx';
 
 const useStyles = makeStyles({
