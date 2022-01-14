4572ac232ce6b78b53ed39c13b76a7785bf83db1
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2022-01-14 13:13:00 -0500

    bundle fallback lang translations

--- src/i18n.js
+++ src/i18n.js
@@ -1,16 +1,22 @@
 import i18n from 'i18next';
 import Backend from 'i18next-http-backend';
+import ChainedBackend from 'i18next-chained-backend';
+import resourcesToBackend from 'i18next-resources-to-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+import translationEN from 'shc-web-verifier-locales/en/translation.json';
+
+const bundledResources = { en: { translation: translationEN } };
 
 i18n
   // load translation using http -> see /public/locales
-  .use(Backend)
+  .use(ChainedBackend)
   // detect user language
   .use(LanguageDetector)
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
   .init({
+    supportedLngs: ['en'],
     fallbackLng: 'en',
     debug: true,
 
@@ -18,7 +24,13 @@ i18n
       escapeValue: false, // not needed for react as it escapes by default
     },
     backend: {
-      loadPath: `${process.env.PUBLIC_URL}/locales/{{lng}}/{{ns}}.json`
+      backends: [
+        Backend,
+        resourcesToBackend(bundledResources)
+      ],
+      backendOptions: [{
+        loadPath: `${process.env.PUBLIC_URL}/locales/{{lng}}/{{ns}}.json`
+      }]
     }
   });
 
