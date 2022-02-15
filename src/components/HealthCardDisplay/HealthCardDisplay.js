c111ad3286d286ae889761c8302c3a7844411278
mickohanlon23 <mohanlon@mitre.org>
2022-02-15 09:06:15 -0500

    Change invalid vaccination series text to _Vaccination status undetermined_

--- src/components/HealthCardDisplay/HealthCardDisplay.js
+++ src/components/HealthCardDisplay/HealthCardDisplay.js
@@ -197,7 +197,7 @@ const HealthCardDisplay = () => {
                 style={{ height: '1.5rem', marginRight: '1rem' }}
               />
               <Typography variant="h6" className={validPrimarySeries ? styles.verifiedText : styles.unverifiedText}>
-                {validPrimarySeries ? t('healthcarddisplay.Valid vaccination series') : t('healthcarddisplay.Could not validate status of vaccination series')}
+                {validPrimarySeries ? t('healthcarddisplay.Valid vaccination series') : t('healthcarddisplay.Vaccination status undetermined')}
               </Typography>
             </Box>
             <Box
