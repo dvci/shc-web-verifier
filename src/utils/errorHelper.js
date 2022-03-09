bef2e0d134132b1d43fe889d53fb1649a272308a
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2022-03-09 17:12:33 -0500

    add fatal error fallback, fix async state errors, overlay page error

--- src/utils/errorHelper.js
+++ src/utils/errorHelper.js
@@ -1,15 +1,13 @@
 /* eslint-disable no-console */
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import {
-  Box, Typography, Button
-} from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import HeroBar from 'components/HeroBar';
 
 const resetErrorHandler = () => {
   localStorage.clear();
-}
+};
 
 const ErrorFallback = ({ resetErrorBoundary }) => {
   const history = useHistory();
@@ -18,9 +16,17 @@ const ErrorFallback = ({ resetErrorBoundary }) => {
   return (
     <>
       <HeroBar hasError />
-      <Box display="flex" width="100%" flexDirection="column" alignItems="center" marginTop="2em">
+      <Box
+        display="flex"
+        width="100%"
+        flexDirection="column"
+        alignItems="center"
+        marginTop="2em"
+      >
         <Box sx={{ maxWidth: '750px' }}>
-          <Typography variant="h6">{t('error.An unexpected error occured.')}</Typography>
+          <Typography variant="h6">
+            {t('error.An unexpected error occurred.')}
+          </Typography>
           <Button
             type="button"
             onClick={() => {
@@ -37,7 +43,4 @@ const ErrorFallback = ({ resetErrorBoundary }) => {
   );
 };
 
-export {
-  ErrorFallback,
-  resetErrorHandler
-};
+export { ErrorFallback, resetErrorHandler };
