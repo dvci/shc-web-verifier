2c23c296eb2afa86d12ad5f0d3d8f67553628437
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2022-03-16 11:09:08 -0400

    Dvci 125 make pages mobile friendly (#23)
    * responsive header bar
    
    * responsive herobar
    
    * responsive landing
    
    * fix header
    
    * fix header logo alignment
    
    * refactor health card display
    
    * refactor health card display
    
    * responsive qr scan
    
    * responsive fonts, fix spacing
    
    * delete test components
    
    * responsive display page
    
    * responsive vaccine display
    
    * vaccine card errors, healthcard display errors
    
    * fix lint error
    
    * multi-QR scanning responsive
    
    * stretch QR scan width
    
    * fix vaccine display width
    
    * Change not valid text.
    
    Co-authored-by: Mick O'Hanlon <mohanlon@mitre.org>

--- src/components/HealthCardDisplay/HealthCardDisplay.test.js
+++ src/components/HealthCardDisplay/HealthCardDisplay.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import axios from 'axios';
-import { act } from 'react-dom/test-utils';
 import ThemeProvider from 'components/ThemeProvider';
 import { QrDataContext } from 'components/QrDataProvider';
 import * as qrHelpers from 'utils/qrHelpers';
@@ -61,66 +59,7 @@ const patientData = {
   ]
 };
 
-const tradenames = `<?xml version="1.0"?>
-<productnames>
-<prodInfo>
-<Name>CDC Product Name</Name>
-<Value>Moderna COVID-19 Vaccine (non-US Spikevax)</Value>
-<Name>Short Description</Name>
-<Value>COVID-19, mRNA, LNP-S, PF, 100 mcg/0.5 mL dose</Value>
-<Name>CVXCode</Name>
-<Value>207 </Value>
-<Name>Manufacturer</Name>
-<Value>Moderna US, Inc.</Value>
-<Name>MVX Code</Name>
-<Value>MOD </Value>
-<Name>MVX Status</Name>
-<Value>Active</Value>
-<Name>Product name Status</Name>
-<Value>Active</Value>
-<Name>Last Updated</Name>
-<Value>7/13/2021</Value>
-</prodInfo>
-<prodInfo>
-<Name>CDC Product Name</Name>
-<Value>Pfizer-BioNTech COVID-19 Vaccine (Non-US COMIRNATY)</Value>
-<Name>Short Description</Name>
-<Value>COVID-19, mRNA, LNP-S, PF, 30 mcg/0.3 mL dose</Value>
-<Name>CVXCode</Name>
-<Value>208 </Value>
-<Name>Manufacturer</Name>
-<Value>Pfizer, Inc</Value>
-<Name>MVX Code</Name>
-<Value>PFR </Value>
-<Name>MVX Status</Name>
-<Value>Active</Value>
-<Name>Product name Status</Name>
-<Value>Active</Value>
-<Name>Last Updated</Name>
-<Value>7/12/2021</Value>
-</prodInfo>
-</productnames>`;
-
-const cvxCodes = `<?xml version='1.0' encoding='ISO-8859-1'?><CVXCodes>
-<CVXInfo>
-<ShortDescription>SARS-COV-2 (COVID-19) vaccine, UNSPECIFIED</ShortDescription>
-<FullVaccinename>SARS-COV-2 (COVID-19) vaccine, UNSPECIFIED</FullVaccinename>
-<CVXCode>213 </CVXCode>
-<Notes>Unspecified code for COVID-19 not to be used to record patient US administration. May be used to record historic US administration if product not known. CVX code 500 should be used to record Non-US vaccine where product is not known.</Notes>
-<Status>Inactive</Status>
-<LastUpdated>7/31/2021</LastUpdated>
-</CVXInfo>
-<CVXInfo>
-<ShortDescription>COVID-19, mRNA, LNP-S, PF, 30 mcg/0.3 mL dose</ShortDescription>
-<FullVaccinename>SARS-COV-2 (COVID-19) vaccine, mRNA, spike protein, LNP, preservative free, 30 mcg/0.3mL dose</FullVaccinename>
-<CVXCode>208 </CVXCode>
-<Notes>FDA BLA 08/23/2021 for adult dose (16+ years). Still under EUA for adolescent doses and presentations. EUA 12/11/2020, 2-dose vaccine. Used to record Pfizer vaccines administered in the US and in non-US locations (includes tradename Comirnaty)</Notes>
-<Status>Active</Status>
-<LastUpdated>9/10/2021</LastUpdated>
-</CVXInfo>
-</CVXCodes>`;
-
-jest.mock('axios');
+jest.mock('../VaccineCard', () => ({ children }) => <>{children}</>);
 
 beforeAll(() => {
   jest.spyOn(React, 'useEffect').mockImplementation((f) => f());
@@ -130,16 +69,23 @@ afterEach(() => {
   jest.spyOn(React, 'useEffect').mockImplementation((f) => f());
 });
 
-const renderHealthCardDisplay = () => {
-  qrHelpers.getPatientData = jest.fn().mockReturnValue(patientData);
+const renderHealthCardDisplay = (
+  displayPatientData,
+  healthCardVerified,
+  healthCardVerifiedError,
+  issuerVerified,
+  validPrimarySeries
+) => {
+  qrHelpers.getPatientData = jest.fn().mockReturnValue(displayPatientData);
   return render(
     <ThemeProvider>
       <QrDataContext.Provider
         value={{
           qrCodes: [],
           setQrCode: jest.fn(),
-          healthCardVerified: { verified: false, error: null },
-          issuerVerified: false,
+          healthCardVerified: { verified: healthCardVerified, error: healthCardVerifiedError },
+          issuerVerified,
+          validPrimarySeries,
         }}
       >
         <HealthCardDisplay />
@@ -148,36 +94,35 @@ const renderHealthCardDisplay = () => {
   );
 }
 
-test('renders health card', () => {
-  renderHealthCardDisplay();
-  const patientName = screen.getByText(/John B. Anyperson/i);
-  expect(patientName).toBeInTheDocument();
-  const immunizationDate = screen.getByText(/2021-01-01/i);
-  expect(immunizationDate).toBeInTheDocument();
+test('renders health card banner verified', () => {
+  renderHealthCardDisplay(patientData, true, null, true, true);
+  expect(screen.getByText(/Verified/i)).toBeInTheDocument();
+  expect(screen.getByText(/Valid SMART® Health Card/i)).toBeInTheDocument();
+  expect(screen.getByText(/Valid vaccination series/i)).toBeInTheDocument();
+  expect(screen.getByText(/Issuer recognized/i)).toBeInTheDocument();
+  expect(screen.getByText(/SCAN QR CODE/i)).toBeInTheDocument();
 });
 
-test('renders health card without performer', () => {
-  // Remove performer from immunization
-  delete patientData.immunizations[0].resource.performer;
-  renderHealthCardDisplay();
-  const patientName = screen.getByText(/John B. Anyperson/i);
-  expect(patientName).toBeInTheDocument();
-  const immunizationDate = screen.getByText(/2021-01-01/i);
-  expect(immunizationDate).toBeInTheDocument();
+test('renders health card banner partially verified', () => {
+  renderHealthCardDisplay(patientData, true, null, false, false);
+  expect(screen.getByText(/Partially Verified/i)).toBeInTheDocument();
+  expect(screen.getByText(/Valid SMART® Health Card/i)).toBeInTheDocument();
+  expect(screen.getByText(/Issuer not recognized/i)).toBeInTheDocument();
+  expect(screen.getByText(/Vaccination series not valid/i)).toBeInTheDocument();
+  expect(screen.getByText(/SCAN QR CODE/i)).toBeInTheDocument();
 });
 
-test('renders immunization display', async () => {
-  React.useEffect.mockRestore();
-  await act(async () => {
-    renderHealthCardDisplay();
-    await axios.get.mockImplementation((url) => {
-      if (url === 'iisstandards_tradename.xml') {
-        return { data: tradenames };
-      }
-      return { data: cvxCodes };
-    });
-    expect(await screen.findAllByText('Pfizer-BioNTech COVID-19 Vaccine (Non-US COMIRNATY)', {}, { timeout: 3000 })).toHaveLength(1);
-    expect(screen.getByText('SARS-COV-2 (COVID-19) vaccine, UNSPECIFIED')).toBeInTheDocument();
-    // screen.debug();
-  });
+test('renders health card banner series invalid', () => {
+  renderHealthCardDisplay(patientData, true, null, true, false);
+  expect(screen.getByText(/Verified/i)).toBeInTheDocument();
+  expect(screen.getByText(/Valid SMART® Health Card/i)).toBeInTheDocument();
+  expect(screen.getByText(/Issuer recognized/i)).toBeInTheDocument();
+  expect(screen.getByText(/Vaccination series not valid/i)).toBeInTheDocument();
+  expect(screen.getByText(/SCAN QR CODE/i)).toBeInTheDocument();
+});
+
+test('renders health card banner invalid', () => {
+  renderHealthCardDisplay(null, false, null, false, false);
+  expect(screen.getByText(/Invalid SMART® Health Card/i)).toBeInTheDocument();
+  expect(screen.getByText(/SCAN QR CODE/i)).toBeInTheDocument();
 });
