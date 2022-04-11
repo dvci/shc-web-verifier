ff23a4a0af81fc7dbc4183631373413c01c720bc
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2022-04-11 14:20:52 -0400

    setup cordova scaffolding, add camera permission config and check (#38)
    * setup cordova scaffolding, add camera permission config and check
    
    * fix android webview video console error

--- src/index.js
+++ src/index.js
@@ -5,14 +5,24 @@ import App from 'components/App';
 import reportWebVitals from './reportWebVitals';
 import './i18n';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Suspense fallback="loading">
-      <App />
-    </Suspense>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const renderReactDom = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Suspense fallback="loading">
+        <App />
+      </Suspense>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+if (window.cordova) {
+  document.addEventListener('deviceready', () => {
+    renderReactDom();
+  }, false);
+} else {
+  renderReactDom();
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
