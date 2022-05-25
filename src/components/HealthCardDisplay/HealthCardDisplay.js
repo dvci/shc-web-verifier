289b339f3be9b9c3bb0b11a6dcbd41755f6bfbc1
sarahmcdougall <smcdougall@mitre.org>
2022-05-25 15:44:37 -0400

    center mult shc link for mobile view

--- src/components/HealthCardDisplay/HealthCardDisplay.js
+++ src/components/HealthCardDisplay/HealthCardDisplay.js
@@ -323,7 +323,7 @@ const HealthCardDisplay = () => {
               <Banners />
             </HealthCardDataProvider>
             <Grid item className={styles.flexCard}>
-              <Box>
+              <Box style={{ textAlign: 'center' }}>
                 <Router>
                   <Switch>
                     <Link
