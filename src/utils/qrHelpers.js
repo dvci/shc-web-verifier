734aad3acc9c5e4398546e8036b45758b6127e45
Sarah McDougall <87094479+sarahmcdougall@users.noreply.github.com>
2022-07-07 15:16:43 -0400

    DVCI 284: Deduplicate immunizations (#74)
    * rename patientBundles --> combinedPatientBundle
    
    * filter duplicate imm entries, add unit test
    
    * lint/prettier fixes
    
    * fix failing build
    
    * add check for vaccineCode system, add unit tests

--- src/utils/qrHelpers.js
+++ src/utils/qrHelpers.js
@@ -52,6 +52,23 @@ const extractImmunizations = (bundle) => {
   return immunizationResources;
 };
 
+const filterDuplicateImmunizations = (resources) => {
+  // filter immunization resources to those with unique
+  // code, system, and occurrence dates
+  const filteredResources = resources.filter(
+    (r, index, self) => r.resource.resourceType !== 'Immunization'
+      || self.findIndex(
+        (e) => e.resource.occurrenceDateTime
+        === r.resource.occurrenceDateTime
+        && e.resource.vaccineCode.coding[0].code
+        === r.resource.vaccineCode.coding[0].code
+        && e.resource.vaccineCode.coding[0].system
+        === r.resource.vaccineCode.coding[0].system
+      ) === index
+  );
+  return filteredResources;
+};
+
 const extractPatientData = (card) => {
   const bundle = JSON.parse(card).vc.credentialSubject.fhirBundle;
   const patient = bundle.entry.find((entry) => entry.resource.resourceType === 'Patient').resource;
@@ -104,5 +121,6 @@ export {
   getPatientData,
   getPayload,
   getCredential,
-  getIssuerDisplayName
+  getIssuerDisplayName,
+  filterDuplicateImmunizations
 };
