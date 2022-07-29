861742172803e60dd4e5c6e96dde9048c94805dc
Zara A Masino <ZMASINO@MITRE.ORG>
2022-07-29 15:07:17 -0400

    cleanup before fiddling with the graph

--- clicky/clickyDataCompiling.py
+++ clicky/clickyDataCompiling.py
@@ -1,21 +1,11 @@
 import requests
-import datetime
 import json
 import pandas as pd
 from datetime import date
-from matplotlib import pyplot
 from os.path import exists
 import matplotlib.pyplot as plt 
  
-
-
-# things to pass in/define outside:
-    # the name of the old data file
-    # the link to the new clicky data
-    # the name that you are saving to (graph and json)
-
-url = "https://api.clicky.com/api/stats/4?site_id=101369228&sitekey=6d6a506f44d45a59&type=visitors-list&date=last-week&output=json"
-
+ 
 # successfully handles if there is no file existing
 def getOldData():
     if exists('ClickyJson.json'):
@@ -32,10 +22,6 @@ def getOldData():
     return data
         
 
-
-
-
-
 # still broken because of the SSL issue
 def getNewData(clickyUrl):
     response = requests.get(clickyUrl, verify=False)
@@ -46,20 +32,6 @@ def getNewData(clickyUrl):
     return newDataList
 
 
-'''what it should look like right now: 
-        [
-          { "time":"1656520164", "time_pretty":"Wed Jun 29 2022, 12:29p", "time_total":"84", "ip_address":"24.34.108.0", "uid":"1313781915", "session_id":"247859048", "actions":"2", "total_visits":"1", "first_visit": {"time":"1656520164", "session_id":"247859048"}, "landing_page":"http://dvci.github.io/shc-web-verifier/", "web_browser":"Google Chrome 103.0", "operating_system":"Mac OS X", "screen_resolution":"1792x1120", "javascript":"1", "language":"English", "referrer_type":"direct", "geolocation":"Arlington, Massachusetts, USA", "country_code":"us", "latitude":"42.4188", "longitude":"-71.1557", "hostname":"comcast.net", "organization":"Comcast Cable", "stats_url":"https://clicky.com/stats/visitors-actions?site_id=101369228&session_id=247859048"},
-          { "time":"1656519562", "time_pretty":"Wed Jun 29 2022, 12:19p", "time_total":"992", "ip_address":"24.34.108.0", "uid":"3166136560", "session_id":"247853892", "actions":"4", "total_visits":"1", "first_visit": {"time":"1656519562", "session_id":"247853892"}, "landing_page":"http://dvci.github.io/shc-web-verifier/", "web_browser":"Safari 15.2", "operating_system":"Mac OS X", "screen_resolution":"1920x1200", "javascript":"1", "language":"English", "referrer_type":"direct", "geolocation":"Arlington, Massachusetts, USA", "country_code":"us", "latitude":"42.4188", "longitude":"-71.1557", "hostname":"comcast.net", "organization":"Comcast Cable", "stats_url":"https://clicky.com/stats/visitors-actions?site_id=101369228&session_id=247853892"},
-          { "time":"1656519103", "time_pretty":"Wed Jun 29 2022, 12:11p", "time_total":"122", "ip_address":"73.149.20.0", "uid":"951136834", "session_id":"247850157", "actions":"1", "total_visits":"1", "first_visit": {"time":"1656519103", "session_id":"247850157"}, "landing_page":"http://dvci.github.io/shc-web-verifier/", "web_browser":"Microsoft Edge 102", "operating_system":"Mac OS X", "screen_resolution":"1792x1120", "javascript":"1", "language":"English", "referrer_type":"direct", "geolocation":"Cambridge, Massachusetts, USA", "country_code":"us", "latitude":"42.3649", "longitude":"-71.0987", "hostname":"comcast.net", "organization":"Comcast Cable", "stats_url":"https://clicky.com/stats/visitors-actions?site_id=101369228&session_id=247850157"},
-          { "time":"1656518793", "time_pretty":"Wed Jun 29 2022, 12:06p", "time_total":"602", "ip_address":"216.10.170.0", "uid":"2415498954", "session_id":"247847735", "actions":"1", "total_visits":"1", "first_visit": {"time":"1656518793", "session_id":"247847735"}, "landing_page":"http://dvci.github.io/shc-web-verifier/", "web_browser":"Google Chrome 103.0", "operating_system":"Mac OS X", "screen_resolution":"1680x1050", "javascript":"1", "language":"English", "referrer_type":"direct", "geolocation":"Laconia, New Hampshire, USA", "country_code":"us", "latitude":"43.5728", "longitude":"-71.4787", "hostname":"atlanticbb.net", "organization":"Atlantic Broadband", "stats_url":"https://clicky.com/stats/visitors-actions?site_id=101369228&session_id=247847735"}
-        ]
-'''
-
-
-
-# kind of just testing here with accessing the right parts of the dictionary 
-# steps: take new data and extract the info you need
-# add it to the old data 
 def merge(newData, oldData):
     uniqueVisitors = []
     oldData["visitorData"]['numTotalVisitors'].append(len(newData))
@@ -74,20 +46,6 @@ def merge(newData, oldData):
     return oldData       
 
 
-'''
-now new dict should look like this I think?:
-clickyData = {
-    "visitorData": {
-        'numTotalVisitors':[3,12,5,4],
-        'numUniqueVisitors':[2,10,5,4],
-    },
-    "dates": ['2022-7-3','2022-7-10','2022-7-17','2022-7-24']
-}
-'''
-
-# allData["visitorData"]['numTotalVisitors']  this does access the right numbers
-# this seems to work but I haven't saved it anywhere, check out that link you saw the other day
-# and see if that works
 def plotData(allData):
     # Declaring the points for first line plot
     X1 = allData['dates']
@@ -106,27 +64,11 @@ def plotData(allData):
     plt.show() 
 
 
-
-# save graph somewhere? yea that would make sense since this is automated and someone would want to 
-# be able to just check it when they need to
-
-# save the info as json somewhere
 def saveJson(allData):
     with open('ClickyJson.json', 'w') as json_file:
         json.dump(allData, json_file)
 
-# call the saves all at once
+
 def saveAll(allInfo):
     plotData(allInfo)
     saveJson(allInfo)
-
-# things left to do: 
-    # clean up code
-    # figure out where/how to store the graph
-    # clean up where everything is stored
-    # test
-
-
-# example call: 
-    # combinedData = mergeData(getNewData(url), getOldData())
-    # saveAll(combinedData)
