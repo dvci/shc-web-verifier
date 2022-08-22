054f85baf8f39a57e3eec25fb232e4627693f735
sarahmcdougall <smcdougall@mitre.org>
2022-08-22 08:57:56 -0400

    update workflow, script documentation

--- clicky/clickyScript.py
+++ clicky/clickyScript.py
@@ -4,13 +4,18 @@ from datetime import date
 from os.path import exists
 import matplotlib.pyplot as plt 
  
+'''
+Retrieves visitor data from the json currently checked into the
+repository.
 
-# retrieves previous data from the json in the repo
-# if there is no previous data, it creates a template to work with and returns that instead
-# returns either as a python dictionary 
+If there is no previous data, creates a template to work with 
+and returns that instead.
+
+Returns: a python dictionary of the outdated visitor data
+'''
 def getOldData():
-    if exists('ClickyJson.json'):
-        with open('ClickyJson.json') as json_file:
+    if exists('clickyData.json'):
+        with open('clickyData.json') as json_file:
             oldDataDict = json.load(json_file)
     else:
         oldDataDict = { 
@@ -21,9 +26,11 @@ def getOldData():
             'dates': []}
     return oldDataDict
         
+'''
+Retrieves the new visitor data from the Clicky url.
 
-# retrieves the new data from the clicky url
-# returns it as a python list
+Returns: python list containing new data from the clicky url
+'''
 def getNewData(clickyUrl):
     # this is a bandaid solution for the SSL, requires further attention
     # would need the certificates to be in place and removal of "verify=False"
@@ -32,8 +39,14 @@ def getNewData(clickyUrl):
     newDataList = json[0]['dates'][0]['items']
     return newDataList
 
-# takes the information in the new data and retrieves the relevant parts
-# adds those parts to the old data
+'''
+Appends the relevant parts of the new visitor data to the existing
+visitor data currently checked into the repository. This includes
+the new number of visits, new IP addresses, new unique visits, and
+the dates of the new visits.
+
+Returns: the merged data
+'''
 def merge(newData, oldData):
     numUniqueVisits = []
     oldData["visitData"]['numTotalVisits'].append(len(newData))
@@ -47,16 +60,19 @@ def merge(newData, oldData):
     oldData["dates"].append(d)
     return oldData       
 
-# plot the information on a graph
-# save the graph to file
+'''
+Plots the information on a graph and saves the graph to file.
+'''
 def plotData(allData):
     X1 = allData['dates']
-    Y1 = allData["visitData"]['numTotalVisits']
+    Y1 = allData['visitData']['numTotalVisits']
     fig = plt.figure(figsize=(10,5))
-    plt.plot(X1, Y1, 'o-', label = "Total Visits") 
+    plt.plot(X1, Y1, 'o-', label = 'Total Visits') 
+
     X2 = allData['dates']
-    Y2 = allData["visitData"]['numUniqueVisits']
-    plt.plot(X2, Y2, 'o-', label = "Unique Visits") 
+    Y2 = allData['visitData']['numUniqueVisits']
+    plt.plot(X2, Y2, 'o-', label = 'Unique Visits') 
+    
     plt.xlabel('Date') 
     plt.ylabel('Number of Visits') 
     plt.title('SHC Verifier Use') 
@@ -64,12 +80,16 @@ def plotData(allData):
     plt.grid()
     fig.savefig('clickyGraph.jpg', bbox_inches='tight', dpi=150)
 
-# save the data to a json file
+'''
+Saves the data to a json file.
+'''
 def saveJson(allData):
-    with open('ClickyJson.json', 'w') as json_file:
+    with open('clickyData.json', 'w') as json_file:
         json.dump(allData, json_file, indent=4)
 
-# save the graph and json at the same time
+'''
+Saves the graph and json at the same time
+'''
 def saveAll(allInfo):
     saveJson(allInfo)
     plotData(allInfo)
--- clicky/runClickyScript.py
+++ clicky/runClickyScript.py
@@ -8,12 +8,3 @@ def main(url):
 
 
 main(clickyUrl)
-
-'''
-if ClickyJson exists and is empty, an error will occur 
-    shouldn't run into it but could add error handling
-might want to implement something to counter if multiple runs on one day yield different numbers
-    (ex: ran once automatically and then after we choose to run it manually for some reason)
-you would want to check if the date is already there, if it is then use the bigger number
-    presumably that would be the more accurate number, taken at a later time
-    '''
