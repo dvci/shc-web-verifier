ad78ff8b908d5f62276bf07f5c061d37f4e8e52e
Neelima Karipineni <1413472+nkarip@users.noreply.github.com>
2022-08-24 11:49:35 -0400

    clarify multiple tradename behavior

--- src/components/VaccineCard/iisstandards.js
+++ src/components/VaccineCard/iisstandards.js
@@ -39,7 +39,8 @@ function processTradenames(xmlDoc) {
   let prodInfo = prodInfos.iterateNext();
   const tn = {};
   while (prodInfo) {
-    if (tn[prodInfo.children[5].textContent.trim()]) {
+    if (tn[prodInfo.children[5].textContent.trim()]) { 
+      // if >1 tradename for a cvx, then use short description which should be same across all
       tn[prodInfo.children[5].textContent.trim()] = prodInfo.children[3].textContent;
     } else {
       tn[prodInfo.children[5].textContent.trim()] = prodInfo.children[1].textContent;
